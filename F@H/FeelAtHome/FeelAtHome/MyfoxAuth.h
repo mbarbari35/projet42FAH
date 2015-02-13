6733feed8ec94329ca431c54bd1e53314f78fabd
Michael BARBARIN <mbarbari@e2r8p15.42.fr>
2015-02-13 12:42:01 +0100

    modification suite a commentaire de robin comme quoi je faisais de la merde :)

--- F@H/FeelAtHome/FeelAtHome/MyfoxAuth.h
+++ F@H/FeelAtHome/FeelAtHome/MyfoxAuth.h
@@ -16,20 +16,15 @@
     NSString *PsswdName;
 }
 
++ (void)initialize;
 - (id) init:(NSString*) username withPassword: (NSString*)password;
-
 - (void) init_autorize:(NSString*) username withPassword: (NSString*)password;
 - (void) logout;
-
 - (void) get_request_site: (void(^)(int, NSError*))cb;
-- (void) get_request: (NSString*)type withItemName:(NSString*)item_name withBlock: (void(^)(int, int, NSError*))cb;
-- (void) get_request_spec: (NSString*)type withItemName:(NSString*)item_name withElem1:(NSString*)elem_name1 withElem2:(NSString*)elem_name2 withBlock: (void(^)(int, int, NSError*))cb;
-- (void) set_request_specific_element:(NSString*) typeDevice withNameDevice: (NSString*)nameDevice withState: (bool) state withErrorHandler: (void(^)(NSError*))cb;
-- (void) set_request_heater_new_state:(NSString*) nameHeater withState: (bool) state withErrorHandler: (void(^)(NSError*))cb;
-- (void) set_request_shutter_new_state:(NSString*) nameShutter withState: (bool) state withErrorHandler: (void(^)(NSError*))cb;
-- (void) set_request_electric_group_new_state:(NSString*) nameG_Elec withState: (bool) state withErrorHandler: (void(^)(NSError*))cb;
-
-
-
+- (void) list_devices: (NSString*)type withBlock: (void(^)(NSArray*, NSError*))cb;
+- (void) set_request_specific_element:(NSString*) typeDevice withIdDevice: (NSInteger)deviceId withState: (bool) state withErrorHandler: (void(^)(NSError*))cb;
+- (void) set_request_heater_new_state:(NSInteger) heaterId withState: (bool) state withErrorHandler: (void(^)(NSError*))cb;
+- (void) set_request_shutter_new_state:(NSInteger)shutterId withState: (bool) state  withErrorHandler: (void(^)(NSError*))cb;
+- (void) set_request_electric_group_new_state:(NSInteger)G_ElecId withState: (bool) state withErrorHandler: (void(^)(NSError*))cb;
 
 @end
--- F@H/FeelAtHome/FeelAtHome/MyfoxAuth.m
+++ F@H/FeelAtHome/FeelAtHome/MyfoxAuth.m
@@ -21,23 +21,28 @@ enum {
 
 @implementation MyfoxAuth
 
++ (void)initialize
+{
+    if (self == [MyfoxAuth class])
+    {
+        [[NXOAuth2AccountStore sharedStore] setClientID:@"fd7528601d0a09a72221de7cfdddeaaa"
+                                                 secret:@"nsKGMQpfGT6Hb599XxD4AgyWnErAmR64"
+                                       authorizationURL:[NSURL URLWithString: @"https://api.myfox.me/oauth2/authorize"]
+                                               tokenURL:[NSURL URLWithString: @"https://api.myfox.me/oauth2/token"]
+                                            redirectURL:[NSURL URLWithString: @"https://localhost/"]
+                                         forAccountType:@"myfox"];
+    }
+}
+
 - (id) init:(NSString*) username withPassword: (NSString*)password
 {
     UserName = username;
     PsswdName = password;
-    //[self init_autorize:UserName withPassword:PsswdName];
     return (self);
 }
 
 - (void) init_autorize:(NSString*) username withPassword: (NSString*)password
 {
-    [[NXOAuth2AccountStore sharedStore] setClientID:@"fd7528601d0a09a72221de7cfdddeaaa"
-                                             secret:@"nsKGMQpfGT6Hb599XxD4AgyWnErAmR64"
-                                   authorizationURL:[NSURL URLWithString: @"https://api.myfox.me/oauth2/authorize"]
-                                           tokenURL:[NSURL URLWithString: @"https://api.myfox.me/oauth2/token"]
-                                        redirectURL:[NSURL URLWithString: @"https://localhost/"]
-                                     forAccountType:@"myfox"];
-
     [[NXOAuth2AccountStore sharedStore] requestAccessToAccountWithType:@"myfox"
                                                               username:username // TODO : Fill username password
                                                               password:password];
@@ -52,7 +57,6 @@ enum {
 {
     [self init_autorize: UserName withPassword: PsswdName];
     
-    NSLog(@"On rentre dans la fonction request site et on se prepare a envoyer la valeur!");
     [NXOAuth2Request performMethod:@"GET"
                         onResource:[NSURL URLWithString:@"https://api.myfox.me/v2/client/site/items"]
                    usingParameters:nil
@@ -80,7 +84,13 @@ enum {
                        else if([object isKindOfClass:[NSDictionary class]])
                        {
                            NSDictionary *results = object;
-                           cb([results[@"siteId"] integerValue], nil);
+                           if([results[@"status"] isEqualToString: @"OK"]) {
+                               NSLog(@"Error X155.0: %s", "Status communication KO with heater");
+                               cb(0, [NSError errorWithDomain:errDomain code:KO_CONNECTION userInfo:nil]);
+                               return ;
+                           }
+                           else
+                               cb([results[@"payload"][@"items"][0][@"siteId"] integerValue], nil);
                        }
                        else {
                            NSLog(@"Error X100.21: the data receive not a dictionary data");
@@ -89,64 +99,11 @@ enum {
                    }];
 }
 
-- (void) get_request: (NSString*)type withItemName:(NSString*)item_name withBlock: (void(^)(int, int, NSError*))cb
-{
-    [self get_request_site: ^(int siteId, NSError *error_site) {
-        if (error_site) {
-            cb(0, 0, error_site);
-            return ;
-        }
-        NSString *add_request;
-        
-        add_request = [NSString stringWithFormat:@"%@%d%@%@%@", @"https://api.myfox.me:443/v2/site/" ,siteId, @"/device/data/", type, @"/items"];
-        
-        if ([type  isEqual: @"light"] || [type  isEqual: @"heater"] || [type  isEqual: @"shutter"])
-        {
-            NSLog(@"Error X200.12: %@ type does not exist !", type);
-            cb(0, 0, [NSError errorWithDomain:errDomain code:UNKNOWN_TYPE userInfo:@{@"unknownType": type}]);
-        }
-        
-        [NXOAuth2Request performMethod:@"GET"
-                            onResource:[NSURL URLWithString:add_request]
-                       usingParameters:nil
-                           withAccount:[[[NXOAuth2AccountStore sharedStore] accountsWithAccountType: @"myfox"] firstObject]
-                   sendProgressHandler:nil
-                       responseHandler:^(NSURLResponse *response, NSData *responseData, NSError *error)
-         {
-             if (error) {
-                 cb(0, 0, error);
-                 return ;
-             }
-             NSError *err2 = nil;
-             id object = [NSJSONSerialization
-                          JSONObjectWithData:responseData
-                          options:0
-                          error:&err2];
-             if(err2)
-             {
-                 NSLog(@"Error X100.1: cannot receive a JSON format : %@", response);
-                 cb(0, 0, err2);
-             }
-             else if([object isKindOfClass:[NSDictionary class]])
-             {
-                 NSDictionary *results = object;
-                 cb([(NSNumber*)results[@"deviceId"] intValue], [(NSNumber*)results[@"siteId"] intValue], nil);
-             }
-             else
-             {
-                 NSLog(@"Error X100.22: %s", "the data receive not a dictionary data");
-                 cb(0, 0, [NSError errorWithDomain:errDomain code:INVALID_DATA userInfo:nil]);
-             }
-         }];
-        
-    }];
-}
-
-- (void) get_request_spec: (NSString*)type withItemName:(NSString*)item_name withElem1:(NSString*)elem_name1 withElem2:(NSString*)elem_name2 withBlock: (void(^)(int, int, NSError*))cb
+- (void) list_devices: (NSString*)type withBlock: (void(^)(NSArray*, NSError*))cb
 {
     [self get_request_site: ^(int siteId, NSError* error_site) {
         if (error_site){
-            cb(0, 0, error_site);
+            cb(nil, error_site);
             return ;
         }
         NSString *add_request;
@@ -156,7 +113,7 @@ enum {
         if ([type  isEqual: @"light"] || [type  isEqual: @"heater"] || [type  isEqual: @"shutter"] || [type  isEqual: @"electric"])
         {
             NSLog(@"Error X200.12: %@ type does not exist !", type);
-            cb(0, 0, [NSError errorWithDomain:errDomain code:UNKNOWN_TYPE userInfo:@{@"unknownType": type}]);
+            cb(nil, [NSError errorWithDomain:errDomain code:UNKNOWN_TYPE userInfo:@{@"unknownType": type}]);
         }
         
         [NXOAuth2Request performMethod:@"GET"
@@ -167,7 +124,7 @@ enum {
                        responseHandler:^(NSURLResponse *response, NSData *responseData, NSError *error)
          {
              if (error) {
-                 cb(0, 0, error);
+                 cb(nil, error);
                  return ;
              }
              NSError *err2 = nil;
@@ -178,24 +135,30 @@ enum {
              if(err2)
              {
                  NSLog(@"Error X100.1: cannot receive a JSON format : %@", response);
-                 cb(0, 0, err2);
+                 cb(nil, err2);
              }
              else if([object isKindOfClass:[NSDictionary class]])
              {
                  NSDictionary *results = object;
-                 cb([(NSNumber*)results[(const NSString*)elem_name1] intValue], [(NSNumber*)results[(const NSString*)elem_name2] intValue], nil);
+                 if([results[@"status"] isEqualToString: @"OK"]) {
+                     NSLog(@"Error X155.0: %s", "Status communication KO with heater");
+                     cb(nil, [NSError errorWithDomain:errDomain code:KO_CONNECTION userInfo:nil]);
+                 }
+                 else
+                     cb(results[@"payload"][@"items"], nil);
              }
              else
              {
                  NSLog(@"Error X100.22: %s", "the data receive not a dictionary data");
-                 cb(0, 0, [NSError errorWithDomain:errDomain code:INVALID_DATA userInfo:nil]);
+                 cb(nil, [NSError errorWithDomain:errDomain code:INVALID_DATA userInfo:nil]);
              }
          }];
         
     }];
+
 }
 
-- (void) set_request_specific_element:(NSString*) typeDevice withNameDevice: (NSString*)nameDevice withState: (bool) state withErrorHandler: (void(^)(NSError*))cb
+- (void) set_request_specific_element:(NSString*) typeDevice withIdDevice: (NSInteger)deviceId withState: (bool) state withErrorHandler: (void(^)(NSError*))cb
 {
     /*
      if state = 1 : on the heater with nameheater
@@ -211,64 +174,61 @@ enum {
      ** modelLabel (string): The device model label
      */
     
-    [self get_request_spec:typeDevice
-              withItemName:nameDevice
-                 withElem1:@"deviceId"
-                 withElem2:@"siteId"
-                 withBlock:^(int siteId, int deviceId, NSError *error_request) {
-                     
-                     if (error_request) {
-                         cb(error_request);
-                         return ;
-                     }
-                     NSString *add_request;
+    [self get_request_site:^(int siteId, NSError *error_site)
+     {
+         if (error_site) {
+             cb(error_site);
+             return ;
+         }
+         NSString *add_request;
          
-                     if (state == TRUE) //On met a on le chauffage
-                         add_request = [NSString stringWithFormat:@"%@%d%@%d%@%@%@", @"https://api.myfox.me:443/v2/site/" ,siteId, @"device/", deviceId, @"/", typeDevice, @"/on"];
-                     else
-                         add_request = [NSString stringWithFormat:@"%@%d%@%d%@%@%@", @"https://api.myfox.me:443/v2/site/" ,siteId, @"device/", deviceId, @"/", typeDevice, @"/off"];
+         if (state == TRUE)
+             add_request = [NSString stringWithFormat:@"%@%d%@%d%@%@%@", @"https://api.myfox.me:443/v2/site/" ,siteId, @"device/", deviceId, @"/", typeDevice, @"/on"];
+         else
+             add_request = [NSString stringWithFormat:@"%@%d%@%d%@%@%@", @"https://api.myfox.me:443/v2/site/" ,siteId, @"device/", deviceId, @"/", typeDevice, @"/off"];
          
-                     
-            [NXOAuth2Request performMethod:@"POST"
-                                onResource:[NSURL URLWithString:add_request]
-                           usingParameters:nil
-                               withAccount:[[[NXOAuth2AccountStore sharedStore] accountsWithAccountType: @"myfox"] firstObject]
-                       sendProgressHandler:nil
-                           responseHandler:^(NSURLResponse *response, NSData *responseData, NSError *error){
-                               if (error) {
-                                   cb(error);
-                                   return ;
-                               }
-                               NSError *err2 = nil;
-                               id object = [NSJSONSerialization
-                                            JSONObjectWithData:responseData
-                                            options:0
-                                            error:&err2];
-                               if(err2)
-                               {
-                                   NSLog(@"Error X100.1: cannot receive a JSON format : %@", response);
-                                   cb(err2);
-                               }
-                               else if([object isKindOfClass:[NSDictionary class]])
-                               {
-                                   NSDictionary *results = object;
-                                   if([results[@"status"] isEqualToString: @"OK"])
-                                   {
-                                       NSLog(@"Error X155.0: %s", "Status communication KO with heater");
-                                       cb([NSError errorWithDomain:errDomain code:KO_CONNECTION userInfo:nil]);
-                                   }
-                               }
-                               else
-                               {
-                                   NSLog(@"Error X100.22: %s", "the data receive not a dictionary data");
-                                   cb([NSError errorWithDomain:errDomain code:INVALID_DATA userInfo:nil]);
-                               }
+         
+         [NXOAuth2Request performMethod:@"POST"
+                             onResource:[NSURL URLWithString:add_request]
+                        usingParameters:nil
+                            withAccount:[[[NXOAuth2AccountStore sharedStore] accountsWithAccountType: @"myfox"] firstObject]
+                    sendProgressHandler:nil
+                        responseHandler:^(NSURLResponse *response, NSData *responseData, NSError *error){
+                            if (error) {
+                                cb(error);
+                                return ;
+                            }
+                            NSError *err2 = nil;
+                            id object = [NSJSONSerialization
+                                         JSONObjectWithData:responseData
+                                         options:0
+                                         error:&err2];
+                            if(err2)
+                            {
+                                NSLog(@"Error X100.1: cannot receive a JSON format : %@", response);
+                                cb(err2);
+                            }
+                            else if([object isKindOfClass:[NSDictionary class]])
+                            {
+                                NSDictionary *results = object;
+                                if([results[@"status"] isEqualToString: @"OK"])
+                                {
+                                    NSLog(@"Error X155.0: %s", "Status communication KO with heater");
+                                    cb([NSError errorWithDomain:errDomain code:KO_CONNECTION userInfo:nil]);
+                                }
+                            }
                             
-                           }];
-                 }];
+                            else
+                            {
+                                NSLog(@"Error X100.22: %s", "the data receive not a dictionary data");
+                                cb([NSError errorWithDomain:errDomain code:INVALID_DATA userInfo:nil]);
+                            }
+                            
+                        }];
+     }];
 }
 
-- (void) set_request_heater_new_state:(NSString*) nameHeater withState: (bool) state withErrorHandler: (void(^)(NSError*))cb
+- (void) set_request_heater_new_state:(NSInteger) heaterId withState: (bool) state withErrorHandler: (void(^)(NSError*))cb
 {
     /*
      if state = 1 : on the heater with nameheater
@@ -284,7 +244,7 @@ enum {
      ** modelLabel (string): The device model label
      */
     
-    [self set_request_specific_element:@"heater" withNameDevice:nameHeater withState:state withErrorHandler:^(NSError *error) {
+    [self set_request_specific_element:@"heater" withIdDevice:heaterId withState:state withErrorHandler:^(NSError *error) {
         if (error) {
             cb(error);
             return ;
@@ -293,7 +253,7 @@ enum {
 }
 
 
-- (void) set_request_shutter_new_state:(NSString*) nameShutter withState: (bool) state  withErrorHandler: (void(^)(NSError*))cb
+- (void) set_request_shutter_new_state:(NSInteger)shutterId withState: (bool) state  withErrorHandler: (void(^)(NSError*))cb
 {
     /*
      if state = 1 : open the shutter with nameshutter
@@ -306,7 +266,7 @@ enum {
      modelLabel (string): The device model label
      */
     
-    [self set_request_specific_element:@"shutter" withNameDevice:nameShutter withState:state withErrorHandler:^(NSError *error) {
+    [self set_request_specific_element:@"shutter" withIdDevice:shutterId withState:state withErrorHandler:^(NSError *error) {
         if (error) {
             cb(error);
             return ;
@@ -315,7 +275,7 @@ enum {
 }
 
 
-- (void) set_request_electric_group_new_state:(NSString*) nameG_Elec withState: (bool) state withErrorHandler: (void(^)(NSError*))cb
+- (void) set_request_electric_group_new_state:(NSInteger)G_ElecId withState: (bool) state withErrorHandler: (void(^)(NSError*))cb
 {
     /*
      if state = 1 : open the shutter with nameshutter
@@ -335,22 +295,19 @@ enum {
         modelLabel (string): The device model label
      }
      */
-    [self get_request_spec:@"electric"
-              withItemName:nameG_Elec
-                 withElem1:@"groupId"
-                 withElem2:@"siteId"
-                 withBlock:^(int siteId, int groupId, NSError *error_request)
+    [self get_request_site:^(int siteId, NSError *error_site)
      {
-         if (error_request){
-             cb(error_request);
+         if (error_site) {
+             cb(error_site);
              return ;
          }
          NSString *add_request;
          
+         
          if (state == TRUE) //Open the electric group
-             add_request = [NSString stringWithFormat:@"%@%d%@%d%@", @"https://api.myfox.me:443/v2/site/" ,siteId, @"group/", groupId, @"/electric/on"];
+             add_request = [NSString stringWithFormat:@"%@%d%@%d%@", @"https://api.myfox.me:443/v2/site/" ,siteId, @"group/", G_ElecId, @"/electric/on"];
          else // Close the electric group
-             add_request = [NSString stringWithFormat:@"%@%d%@%d%@", @"https://api.myfox.me:443/v2/site/" ,siteId, @"device/", groupId, @"/electric/off"];
+             add_request = [NSString stringWithFormat:@"%@%d%@%d%@", @"https://api.myfox.me:443/v2/site/" ,siteId, @"group/", G_ElecId, @"/electric/off"];
          
          
          [NXOAuth2Request performMethod:@"POST"
@@ -359,6 +316,10 @@ enum {
                             withAccount:[[[NXOAuth2AccountStore sharedStore] accountsWithAccountType: @"myfox"] firstObject]
                     sendProgressHandler:nil
                         responseHandler:^(NSURLResponse *response, NSData *responseData, NSError *error){
+                            if (error) {
+                                cb(error);
+                                return ;
+                            }
                             NSError *err2 = nil;
                             id object = [NSJSONSerialization
                                          JSONObjectWithData:responseData
@@ -372,11 +333,13 @@ enum {
                             else if([object isKindOfClass:[NSDictionary class]])
                             {
                                 NSDictionary *results = object;
-                                if([results[@"status"] isEqualToString: @"OK"]) {
+                                if([results[@"status"] isEqualToString: @"OK"])
+                                {
                                     NSLog(@"Error X155.0: %s", "Status communication KO with heater");
                                     cb([NSError errorWithDomain:errDomain code:KO_CONNECTION userInfo:nil]);
                                 }
                             }
+                            
                             else
                             {
                                 NSLog(@"Error X100.22: %s", "the data receive not a dictionary data");
@@ -386,8 +349,4 @@ enum {
                         }];
      }];
 }
-
-
-
-
 @end
